b1519f6d60383de2e90a2d2efd200aab0d3c43a0
mjdthif <mjd.thif@icloud.com>
2020-09-11 11:22:21 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,98 @@
+// local reviews data
+const reviews = [
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text:
+      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "anna johnson",
+    job: "web designer",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text:
+      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text:
+      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text:
+      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
+
+// Select items from the DOC
+
+const img =  document.getElementById("person-img")
+const author =  document.getElementById("author")
+const info=  document.getElementById("info")
+const job =  document.getElementById("job")
+
+
+
+// Select the buttons...
+const  prevBtn =  document.querySelector(".prev-btn")
+const  nextBtn =  document.querySelector(".next-btn")
+const  randomBtn =  document.querySelector(".random-btn")
+const review =  document.querySelector(".review")
+
+// set the starting item for the review.
+let currentItem  = 0; 
+
+// create an event listener to make sure the DOC is loaded. 
+window.addEventListener('DOMContentLoaded', function(){
+  getItem(currentItem)
+})
+
+// create a function to capture item's info. 
+function getItem(person){
+  const item =  reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  info.textContent =  item.text; 
+  job.textContent =  item.job;
+}
+
+// add an event listener to the buttons. 
+
+nextBtn.addEventListener('click', function(){
+    currentItem +=1;
+    if(currentItem > reviews.length - 1){
+      currentItem = 0
+    }
+    getItem(currentItem);
+})
+
+ 
+//
+prevBtn.addEventListener('click', function(){
+  currentItem -=1;
+  if(currentItem < 0 ){
+    currentItem = reviews.length - 1;
+  }
+  getItem(currentItem);
+})
+randomBtn.addEventListener('click', function(){
+  currentItem =  Math.floor(Math.random()*reviews.length);
+  getItem(currentItem)
+})
+
+
+
